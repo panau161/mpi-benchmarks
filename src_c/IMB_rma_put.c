e38f9245dcd5dff3cd6ba98768ed725a6fed6ab3
Tobias Baumann <github@panau.icu>
2026-01-09 16:14:08 +0100

    IMB_rma_put/get: check ranks first

--- src_c/IMB_rma_put.c
+++ src_c/IMB_rma_put.c
@@ -77,6 +77,11 @@ void IMB_rma_put(struct comm_info* c_info, int size,
     defect = 0;
 #endif
 
+    if (c_info->rank < 0) {
+        *time = res_time;
+        return;
+    }
+
     if (c_info->rank == c_info->pair0) {
         target = c_info->pair1;
         sender = 1;
@@ -86,9 +91,6 @@ void IMB_rma_put(struct comm_info* c_info, int size,
             /* pair1 acts as origin in bidirectional mode only */
             sender = 1;
         }
-    } else if (c_info->rank < 0) {
-        *time = res_time;
-        return;
     }
 
     MPI_Type_size(c_info->s_data_type, &s_size);
